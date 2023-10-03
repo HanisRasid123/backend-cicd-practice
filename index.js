3f158028d4a76846c0dac2db6a4e616bcafe7bc4
Hanis Rasid <hanis.rasid@healthrfid.com>
2023-10-03 15:08:53 +1100

    add cors middleware and dynamic product endpoint for items

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql2')
+const cors = require('cors')
 
-const PORT = 3000
+const PORT = 8000
 
 app.use(express.json())
+app.use(cors({origin: 'http://localhost:3000'}))
 
 const conn = mysql.createConnection({
   host: 'localhost',
@@ -17,10 +19,22 @@ app.listen(PORT, ()=> {
   console.log(`Server is listening on port ${PORT}...`)
 })
 
-app.get("/user", (req, res)=>{
-  conn.query("SELECT * FROM users;", (err, rows)=>{
+app.get("/products/:id", (req, res)=>{
+  conn.query(`SELECT * FROM products where product_id = ${req.params.id};`, (err, product)=>{
     if (err) {
       res.send({message: "Error querying the database."})
+    }
+    else {
+      console.log(product)
+      res.status(200).send(product)
+    }
+  })
+})
+
+app.get("/products", (req,res) => {
+  conn.query("SELECT * FROM products;", (err, rows) => {
+    if (err) {
+      res.send({message: "Error querying database."})
     }
     else {
       res.status(200).send(rows)
@@ -28,18 +42,18 @@ app.get("/user", (req, res)=>{
   })
 })
 
-app.post("/createUser", (req, res)=>{
-  const {name, password} = req.body
+// app.post("/createUser", (req, res)=>{
+//   const {name, password} = req.body
 
-  if(!name || !password) {
-    res.status(418).send({message: 'We need a name to create a user!'})
-  }
-  else {
-    conn.query(`INSERT INTO users (name, password) values ('${name}','${password}')`, (err) => {
-      if (err) {
-        res.send({message: "Error querying the database"})
-      }
-    })
-    res.status(200).send({message: "Successfully added user to the database"})
-  }
-})
+//   if(!name || !password) {
+//     res.status(418).send({message: 'We need a name to create a user!'})
+//   }
+//   else {
+//     conn.query(`INSERT INTO users (name, password) values ('${name}','${password}')`, (err) => {
+//       if (err) {
+//         res.send({message: "Error querying the database"})
+//       }
+//     })
+//     res.status(200).send({message: "Successfully added user to the database"})
+//   }
+// })
